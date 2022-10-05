3f94fb10b1f372e6b68a9ca0d0a3e878e890ed38
Doğan Can Bakır <dogancanbakir@protonmail.com>
2022-10-05 20:35:53 +0000

    add cli option for proba

--- metamaska/cli.py
+++ metamaska/cli.py
@@ -5,11 +5,12 @@ from metamaska.metamaska import Metamaska
 
 @click.command(no_args_is_help=True)
 @click.option('-m', '--meta', required=True, help = "meta to find form for")
-def main(meta):
+@click.option('-p', '--proba', default=False, help = "meta to find form for")
+def main(meta, proba):
     if not meta:
         raise ValueError("meta is required.")
 
-    print(Metamaska().form(meta))
+    print(Metamaska().form(meta, proba))
 
 
 if __name__ == "__main__":
